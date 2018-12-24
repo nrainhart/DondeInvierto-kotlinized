3bffaa254094fac9082ab6c619e943a7232f3f8f
Nico <nmrainhart@gmail.com>
2018-12-24 13:22:43 -0300

    Refactoriza Antiguedad#evaluarEn para evitar conversiones innecesarias

--- src/main/java/dominio/metodologias/Antiguedad.java
+++ src/main/java/dominio/metodologias/Antiguedad.java
@@ -10,12 +10,11 @@ import java.time.Year;
 public class Antiguedad extends Cuantificador {
 
 	public Antiguedad() {
-	} // PROBLEMA: SE VAN A ESTAR CREANDO MÚLTIPLES INSTANCIAS DE ANTIGUEDAD, CUANDO
-		// UNA SERÍA SUFICIENTE (!!!)
+	} // PROBLEMA: SE VAN A ESTAR CREANDO MÚLTIPLES INSTANCIAS DE ANTIGUEDAD, CUANDO UNA SERÍA SUFICIENTE (!!!)
 
 	@Override
 	public int evaluarEn(Empresa empresa, Year anio) {
-		int antiguedad = Integer.parseInt(anio.toString()) - Integer.parseInt(empresa.anioDeCreacion().toString());
+		int antiguedad = anio.getValue() - empresa.anioDeCreacion().getValue();
 		if (antiguedad < 0) {
 			throw new AntiguedadMenorACeroError("La empresa " + empresa.getNombre() + " no existía en el año " + anio);
 		}
