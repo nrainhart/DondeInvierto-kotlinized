5d40fd4495513a25f5a55ff4428317ba3a800bba
Nico <nmrainhart@gmail.com>
2018-12-27 20:36:15 -0300

    Kotliniza ExpresionOperacion

---
+++ src/main/java/dominio/indicadores/ExpresionOperacion.kt
@@ -0,0 +1,24 @@
+package dominio.indicadores
+
+import dominio.empresas.Empresa
+import java.lang.IllegalStateException
+
+import java.time.Year
+
+class ExpresionOperacion(private val operandoIzq: Expresion,
+                         private val operandoDer: Expresion,
+                         operadorBinario: String) : Expresion {
+
+    private val operadorAritmetico: OperacionAritmetica = when (operadorBinario) {
+        "+" -> OperacionAritmetica.Mas
+        "-" -> OperacionAritmetica.Menos
+        "*" -> OperacionAritmetica.Por
+        "/" -> OperacionAritmetica.Dividido
+        else -> throw IllegalStateException("Las únicas operaciones aceptadas son: [+,-,*,/]")
+    }
+
+    override fun evaluarEn(empresa: Empresa, anio: Year): Int {
+        return operadorAritmetico.applyAsInt(operandoIzq.evaluarEn(empresa, anio), operandoDer.evaluarEn(empresa, anio))
+    }
+
+}
