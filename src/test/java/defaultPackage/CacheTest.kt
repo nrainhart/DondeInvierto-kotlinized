8fed765d546e2fb937c94cac9ee0b9c5f68ac4fa
Nico <nmrainhart@gmail.com>
2018-12-29 14:57:43 -0300

    Kotliniza CacheTest

---
+++ src/test/java/defaultPackage/CacheTest.kt
@@ -0,0 +1,60 @@
+package defaultPackage
+
+import dominio.empresas.ArchivoXLS
+import dominio.empresas.Empresa
+import dominio.indicadores.Indicador
+import dominio.indicadores.IndicadorPrecalculado
+import dominio.usuarios.Usuario
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager
+import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest
+import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps
+import java.time.Year
+import java.util.*
+
+class CacheTest : AbstractPersistenceTest(), WithGlobalEntityManager, TransactionalOps {
+
+    private val usuario = Usuario("admin", "admin")
+    private lateinit var ingresoNeto: Indicador
+    private lateinit var sony: Empresa
+
+    @Before
+    fun setUp() {
+        Usuario.activo = usuario
+        withTransaction {
+            usuario.agregarIndicadores(Arrays.asList(
+                    "INGRESONETO = netooperacionescontinuas + netooperacionesdiscontinuas",
+                    "SALDOCRUDO = cuentarara + fds"))
+        }
+        val indicadores = usuario.indicadores
+        val archivoEjemploIndicadores = ArchivoXLS("src/test/resources/EjemploIndicadores.xls")
+        archivoEjemploIndicadores.leerEmpresas()
+        val empresasParaIndicadores = archivoEjemploIndicadores.empresas
+        ingresoNeto = indicadores[0]
+        sony = empresasParaIndicadores[0]
+    }
+
+    @Test
+    fun elIndicadorCacheaElResultadoLuegoDeCalcularlo() {
+        val resultado = IndicadorPrecalculado(sony, Year.of(2017), 17000)
+        ingresoNeto.evaluarEn(sony, Year.of(2017))
+        assertTrue(ingresoNeto.getResultados().contains(resultado))
+    }
+
+    @Test
+    fun elIndicadorNoCacheaDosVecesElResultadoLuegoDeCalcularlo() {
+        ingresoNeto.evaluarEn(sony, Year.of(2017))
+        ingresoNeto.evaluarEn(sony, Year.of(2017))
+        assertEquals(ingresoNeto.getResultados().size, 1)
+    }
+
+    @Test
+    fun elIndicadorDevuelveElResultadoCorrectoSiEstaCacheado() {
+        ingresoNeto.evaluarEn(sony, Year.of(2017))
+        val resultadoCacheado = ingresoNeto.getResultados()[0]
+        assertEquals(resultadoCacheado.valor, ingresoNeto.evaluarEn(sony, Year.of(2017)))
+    }
+}
