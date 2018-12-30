c1b8a1981b1d21fcc141a2674f866329aaf0279d
Nico <nmrainhart@gmail.com>
2018-12-30 18:14:15 -0300

    Crea Aplicación de Spring con EmpresaController mockeado

---
+++ src/main/java/dondeInvierto/spring_controllers/EmpresaController.kt
@@ -0,0 +1,17 @@
+package dondeInvierto.spring_controllers
+
+import dominio.empresas.Cuenta
+import dominio.empresas.Empresa
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.Year
+
+@RestController
+@RequestMapping("/api/empresas")
+class EmpresaController {
+
+  @GetMapping
+  fun get() = Empresa("10Pines", mutableListOf(Cuenta(Year.of(2018), "ebitda", 200)))
+
+}
