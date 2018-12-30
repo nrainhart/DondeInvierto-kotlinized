283bcc7ea4d69bd83e93ed2cd5a8aa7ce4e65653
Nico <nmrainhart@gmail.com>
2018-12-30 16:29:27 -0300

    Elimina carga de Empresas desde Amazon S3

--- src/main/java/scheduler/SchedulerMain.java
+++ src/main/java/scheduler/SchedulerMain.java
@@ -3,9 +3,6 @@ package scheduler;
 import dominio.usuarios.RepositorioUsuarios;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import server.CargarEmpresas;
 
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.SimpleScheduleBuilder.repeatSecondlyForever;
@@ -13,14 +10,11 @@ import static org.quartz.TriggerBuilder.newTrigger;
 
 public class SchedulerMain {
 
-    final static Logger logger = LoggerFactory.getLogger(SchedulerMain.class);
-
     public static void main(String[] args) throws Exception {
         Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
 
         scheduler.start();
 
-        JobDetail jobDetailCargarEmpresas = newJob(CargarEmpresas.class).build();
         JobDetail jobDetailEliminarIndicadoresPrecalculados = newJob(EliminarIndicadoresPrecalculados.class).build();
 
         Trigger trigger = newTrigger()
@@ -28,13 +22,12 @@ public class SchedulerMain {
                 .withSchedule(repeatSecondlyForever(2592000))
                 .build();
 
-        scheduler.scheduleJob(jobDetailCargarEmpresas, trigger);
         scheduler.scheduleJob(jobDetailEliminarIndicadoresPrecalculados, trigger);
     }
     
     public static class EliminarIndicadoresPrecalculados implements Job {
     	@Override
-        public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        public void execute(JobExecutionContext jobExecutionContext) {
     		new RepositorioUsuarios().eliminarIndicadoresPrecalculados();
        }
     }
