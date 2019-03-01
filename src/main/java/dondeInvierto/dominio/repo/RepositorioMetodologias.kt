4eb61d055c9ab6b1854c5b980cd0939c3f1922fa
Nico <nmrainhart@gmail.com>
2019-03-01 11:48:53 -0300

    Refactoriza métodos de RepositorioMetodologias

--- src/main/java/dondeInvierto/dominio/repo/RepositorioMetodologias.kt
+++ src/main/java/dondeInvierto/dominio/repo/RepositorioMetodologias.kt
@@ -6,13 +6,8 @@ import javax.persistence.Entity
 @Entity
 class RepositorioMetodologias : AbstractLocalRepository<Metodologia>() {
 
-    override fun mensajeEntidadExistenteError(elemento: Metodologia): String {
-        return "Ya existe una metodología con el nombre " + elemento.nombre
-    }
+    override fun mensajeEntidadExistenteError(elemento: Metodologia) = "Ya existe una metodología con el nombre " + elemento.nombre
 
-    fun obtenerPorId(id: Long?): Metodologia {
-        return obtenerTodos().stream().filter { metodologia -> metodologia.id == id }.findFirst()
-                .orElseThrow<RuntimeException>(null)//Estoy seguro que la metodologia va a existir
-    }
+    fun obtenerPorId(id: Long?): Metodologia = obtenerTodos().first { it.id == id }
 
 }
