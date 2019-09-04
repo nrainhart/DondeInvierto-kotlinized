84ff4bcf718ff0435aa5114c9df9be9c3c8173dc
Nico <nmrainhart@gmail.com>
2019-09-04 13:08:50 -0300

    Kotlin 1

--- src/main/java/dondeInvierto/dominio/empresas/Cuenta.kt
+++ src/main/java/dondeInvierto/dominio/empresas/Cuenta.kt
@@ -1,40 +1,39 @@
 package dondeInvierto.dominio.empresas
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.Table
-import java.time.Year
-import java.util.Objects
+    import java.time.Year
+    import java.util.*
 
-@Entity
-@Table(name = "cuentas")
-class Cuenta(val anio: Year, val tipoCuenta: String, valor: Int) {
+    class Cuenta {
 
-    @Id
-    @GeneratedValue
-    private val id: Long? = null
+        val anio: Year
+        val tipoCuenta: String
+        var valor: Int
+            private set
 
-    var valor: Int = valor
-        private set
+        constructor(anio: Year, tipoCuenta: String, valor: Int) {
+            this.anio = anio
+            this.tipoCuenta = tipoCuenta
+            this.valor = valor
+        }
 
-    fun actualizar(cuentaConDatosNuevos: Cuenta) {
-        valor = cuentaConDatosNuevos.valor
+        fun actualizar(valorActualizado: Int) {
+            this.valor = valorActualizado
+        }
+
+        fun esDeTipoYAnio(tipoCuenta: String, anio: Year) = esDeTipo(tipoCuenta) && esDeAnio(anio)
+
+        private fun esDeTipo(tipo: String) = this.tipoCuenta.equals(tipo, ignoreCase = true)
+
+        private fun esDeAnio(anio: Year) = this.anio == anio
+
+        override fun equals(other: Any?): Boolean {
+            return other is Cuenta &&
+                    anio == other.anio &&
+                    tipoCuenta == other.tipoCuenta &&
+                    valor == other.valor
+        }
+
+        override fun hashCode() = Objects.hash(anio, tipoCuenta, valor)
+
     }
 
-    fun esDeTipoYAnio(tipoCuenta: String, anio: Year) =
-      esDeTipo(tipoCuenta) && esDeAnio(anio)
-
-    private fun esDeTipo(tipo: String): Boolean = this.tipoCuenta.equals(tipo, ignoreCase = true)
-
-    private fun esDeAnio(anio: Year): Boolean = this.anio == anio
-
-    override fun equals(other: Any?): Boolean {
-        return other is Cuenta &&
-                anio == other.anio &&
-                tipoCuenta == other.tipoCuenta
-    }
-
-    override fun hashCode(): Int = Objects.hash(anio, tipoCuenta)
-
-}
--- src/main/java/dondeInvierto/dominio/empresas/Empresa.kt
+++ src/main/java/dondeInvierto/dominio/empresas/Empresa.kt
@@ -1,75 +1,34 @@
 package dondeInvierto.dominio.empresas
 
-import dondeInvierto.dominio.indicadores.Indicador
-import excepciones.NoExisteCuentaError
-import java.time.Year
-import javax.persistence.*
+    import excepciones.NoExisteCuentaError
+    import java.time.Year
 
-@Entity
-@Table(name = "empresas")
-class Empresa(val nombre: String,
-              @OneToMany(cascade = [CascadeType.PERSIST])
-              @JoinColumn(name = "empresa_id")
-              private val cuentas: MutableList<Cuenta>
-) {
+    class Empresa {
 
-    @Id
-    @GeneratedValue
-    val id: Long? = null
+        val nombre: String
+        private val cuentas: MutableList<Cuenta>
 
-    fun anioDeCreacion(): Year {
-        return cuentas.map { it.anio }
-                .min()
-                ?: throw NoExisteCuentaError("La empresa no tiene ninguna cuenta, por lo que no se puede calcular el año de creación.")
-    }
-
-    fun cantidadDeCuentas(): Int = cuentas.size
-
-    fun seLlama(nombre: String): Boolean = this.nombre == nombre
-
-    fun registrarCuenta(cuenta: Cuenta) {
-        cuentas.add(cuenta)
-    }
-
-    fun actualizar(empresaConDatosNuevos: Empresa) {
-        for (cuenta in empresaConDatosNuevos.getCuentas()) {
-            if (!cuentas.contains(cuenta))
-                registrarCuenta(cuenta)
-            else {
-                val cuentaAActualizar = cuentas[cuentas.indexOf(cuenta)]
-                cuentaAActualizar.actualizar(cuenta)
-            }
+        constructor(nombre: String, cuentas: MutableList<Cuenta>) {
+            this.nombre = nombre
+            this.cuentas = cuentas
         }
+
+        fun registrarCuenta(cuenta: Cuenta) {
+            cuenta.actualizar(6)
+            cuentas.add(cuenta)
+        }
+
+        fun anioDeCreacion(): Year {
+            return cuentas.map { it.anio }
+                    .min()
+                    ?: throw NoExisteCuentaError("La empresa no tiene ninguna cuenta, por lo que no se puede calcular el año de creación.")
+        }
+
+        fun getValorCuenta(tipoCuenta: String, anio: Year): Int {
+            return cuentas.firstOrNull { cuenta -> cuenta.esDeTipoYAnio(tipoCuenta, anio) }
+                    ?.valor
+                    ?: throw NoExisteCuentaError("No se pudo encontrar la cuenta $tipoCuenta en el año $anio para la empresa ${this.nombre}.")
+        }
+
     }
 
-    fun aniosDeLosQueTieneCuentas(): Set<Year> {
-        return cuentas.map { it.anio }
-                .toSet()
-    }
-
-    fun resultadosParaEstosIndicadores(indicadores: List<Indicador>): List<Cuenta> {
-        return aniosDeLosQueTieneCuentas().flatMap { anio -> resultadosParaEstosIndicadoresSegunAnio(indicadores, anio) }
-    }
-
-    private fun resultadosParaEstosIndicadoresSegunAnio(indicadores: List<Indicador>, anio: Year): List<Cuenta> {
-        return indicadores.filter { it.esAplicableA(this, anio) }
-                .map { indicador -> Cuenta(anio, indicador.nombre, indicador.evaluarEn(this, anio)) }
-        // TODO no se debería representar el resultado de evaluar un Indicador con una Cuenta
-    }
-
-    fun getCuentas(): List<Cuenta> = cuentas
-
-    fun getValorCuenta(tipoCuenta: String, anio: Year): Int {
-        return cuentas.firstOrNull { cuenta -> cuenta.esDeTipoYAnio(tipoCuenta, anio) }
-                //El first podría enmascarar el caso erróneo en el que haya dos cuentas del mismo tipo con valores distintos en el mismo año
-                ?.valor
-                ?: throw NoExisteCuentaError("No se pudo encontrar la cuenta $tipoCuenta en el año $anio para la empresa ${this.nombre}.")
-    }
-
-    override fun equals(other: Any?): Boolean = other is Empresa && this.seLlama(other.nombre)
-
-    override fun hashCode(): Int = nombre.hashCode()
-
-    override fun toString(): String = nombre //Es necesario para que el Selector muestre solo el nombre de la empresa
-
-}
