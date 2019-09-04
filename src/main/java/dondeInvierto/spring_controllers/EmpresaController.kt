0aa65588eec22b9160469f2d4ecfe3bb79e1a304
Nico <nmrainhart@gmail.com>
2019-09-04 14:39:44 -0300

    Spring Boot

--- src/main/java/dondeInvierto/spring_controllers/EmpresaController.kt
+++ src/main/java/dondeInvierto/spring_controllers/EmpresaController.kt
@@ -1,30 +1,28 @@
 package dondeInvierto.spring_controllers
 
-import dondeInvierto.dominio.empresas.Cuenta
 import dondeInvierto.dominio.empresas.Empresa
-import dondeInvierto.dominio.indicadores.Indicador
 import dondeInvierto.repo.EmpresaRepository
 import dondeInvierto.repo.IndicadorRepository
 import excepciones.NoExisteElResultadoBuscadoError
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.web.bind.annotation.*
 
+interface EmpresaRepository: CrudRepository<Empresa, Long>
+
 @RestController
 @RequestMapping("/api/empresas")
-class EmpresaController(@Autowired val empresaRepository: EmpresaRepository,
-                        @Autowired val indicadorRepository: IndicadorRepository
-) {
+class EmpresaController(@Autowired val empresaRepository: EmpresaRepository) {
 
   @GetMapping
-  fun listar(): Iterable<Empresa> = empresaRepository.findAll()
+  fun listar(): List<Empresa> = empresaRepository.findAll().toList()
 
   @GetMapping("/{id}")
-  fun detalle(@PathVariable("id") idEmpresa: Long): List<Cuenta> {
-    val empresa: Empresa = empresaRepository.findByIdOrNull(idEmpresa)
+  fun detalle(@PathVariable("id") idEmpresa: Long): Empresa {
+    return empresaRepository.findByIdOrNull(idEmpresa)
       ?: throw NoExisteElResultadoBuscadoError("No pudo encontrarse el resultado para Empresa[id=$idEmpresa]")
-    val indicadoresAEvaluar: List<Indicador> = indicadorRepository.findAll().toList()
-    val cuentasSeleccionadas = empresa.getCuentas() + empresa.resultadosParaEstosIndicadores(indicadoresAEvaluar)
-    return cuentasSeleccionadas.sortedByDescending { it.anio }
   }
+
 }
+
